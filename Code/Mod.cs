0280ba0f7a20d894decc6f53181e61ab857eccba
algernon-A <aeneas@aloku.net>
2021-12-15 12:57:59 +1100

    Version bump to 0.5.

--- Code/Mod.cs
+++ Code/Mod.cs
@@ -10,7 +10,7 @@ namespace MoreCitizenUnits
     public class MCUMod : IUserMod
     {
         public static string ModName => "More CitizenUnits";
-        public static string Version => "0.4";
+        public static string Version => "0.5";
 
         public string Name => ModName + " " + Version;
         public string Description => Translations.Translate("MCU_DESC");
